e877b2350c6f4034edcd525df1c236d9d630f5f3
ferasatali <ferasatali14@gmail.com>
2024-08-11 16:15:20 +0500

    refactor: Update tab value assignment in discover.vue and exploreCurrentTab initialization in app.ts
    The code changes in discover.vue update the assignment of the tab value, setting it to the result of the store's setExploreCurrentTab function instead of the exploreCurrentTab property. This ensures that the correct tab value is assigned. In app.ts, the exploreCurrentTab initialization is moved below the headerConnnect initialization for better code organization.
    
    This commit message follows the established convention of using a verb in the imperative form ("Update tab value assignment" and "Update exploreCurrentTab initialization") and provides a clear and concise description of the changes made.

--- src/store/app.ts
+++ src/store/app.ts
@@ -9,9 +9,9 @@ import axios from "axios";
 // const axios: any = inject("axios");
 
 export const AppStore = defineStore("app", () => {
-  const exploreCurrentTab = ref(0);
   const navBar = ref("Home");
   const headerConnnect = ref(false);
+  const exploreCurrentTab = ref(0);
   function setNavBar(title: string) {
     navBar.value = title;
   }
@@ -37,9 +37,9 @@ export const AppStore = defineStore("app", () => {
     }
   }
 
-
   return {
     headerConnnect,
+    exploreCurrentTab,
     setNavBar,
     getNavBar,
     setExploreCurrentTab,
